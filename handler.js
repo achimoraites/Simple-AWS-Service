4ead4b77c7bbc1406e24c9e0318070e8f7ef7734
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-29 13:08:35 +0200

    installed dev env

---
+++ handler.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const dynamoDb = require('./dynamodb');
+const uuid = require('uuid');
+
+module.exports.write = (event, context, callback) => {
+  // const data = JSON.parse(event.body)
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      text: 'data.text'
+    }
+  }
+  
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.error(error)
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the todo item.'
+      })
+      return
+    }
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item)
+    }
+    callback(null, response)
+  })
+};
