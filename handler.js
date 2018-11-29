4f866eb1ad620079c101b3a7779590af4bdec5e9
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-29 18:59:10 +0200

    ugly but works

--- handler.js
+++ handler.js
@@ -4,35 +4,70 @@ const dynamoDb = require('./dynamodb');
 const uuid = require('uuid');
 
 module.exports.write = (event, context, callback) => {
-  // const data = JSON.parse(event.body);
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-    Item: {
-      id: uuid.v1(),
-      artist: 'artist'
-    }
-  };
+    // const data = JSON.parse(event.body);
+    // const params = {
+    //   TableName: process.env.DYNAMODB_TABLE,
+    //   Item: {
+    //     id: uuid.v1(),
+    //     artist: 'artist'
+    //   }
+    // };
 
-
-  dynamoDb.put(params, (error) => {
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: {
-          'Content-Type': 'text/plain'
-        },
-        body: 'Couldn\'t create the todo item.'
+// Customized params
+    const params = (artist) => {
+      return {
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: {
+          id: uuid.v1(),
+          artist
+        }
+      };
+     
+    };
+// put the records
+    for(let i=0;i<10;i++) {
+      setTimeout(() => {
+        dynamoDb.put(params(`artist ${uuid.v4()}`), (error) => {
+          if (error) {
+            console.error(error);
+            callback(null, {
+              statusCode: error.statusCode || 501,
+              headers: {
+                'Content-Type': 'text/plain'
+              },
+              body: 'Couldn\'t create the todo item.'
+            });
+            return;
+          }
       });
-      return;
+
+         }, 10);
     }
+
     const response = {
       statusCode: 200,
       body: JSON.stringify(params.Item)
     };
     callback(null, response);
-  });
+      // dynamoDb.put(params, (error) => {
+      //   if (error) {
+      //     console.error(error);
+      //     callback(null, {
+      //       statusCode: error.statusCode || 501,
+      //       headers: {
+      //         'Content-Type': 'text/plain'
+      //       },
+      //       body: 'Couldn\'t create the todo item.'
+      //     });
+      //     return;
+      //   }
+      //   const response = {
+      //     statusCode: 200,
+      //     body: JSON.stringify(params.Item)
+      //   };
+      //   callback(null, response);
+      // });
 
 
 
-};
+    };
