7d66052fc72152ee39fbef4b22daf83c7c115490
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-29 15:45:41 +0200

    tidying up

--- handler.js
+++ handler.js
@@ -4,29 +4,35 @@ const dynamoDb = require('./dynamodb');
 const uuid = require('uuid');
 
 module.exports.write = (event, context, callback) => {
-  // const data = JSON.parse(event.body)
+  // const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Item: {
       id: uuid.v1(),
       artist: 'artist'
     }
-  }
-  
+  };
+
+
   dynamoDb.put(params, (error) => {
     if (error) {
-      console.error(error)
+      console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
+        headers: {
+          'Content-Type': 'text/plain'
+        },
         body: 'Couldn\'t create the todo item.'
-      })
-      return
+      });
+      return;
     }
     const response = {
       statusCode: 200,
       body: JSON.stringify(params.Item)
-    }
-    callback(null, response)
-  })
+    };
+    callback(null, response);
+  });
+
+
+
 };
