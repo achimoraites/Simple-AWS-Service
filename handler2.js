0c36d6849baa7f34078e8e5dd85e09372594ffb9
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-30 12:46:19 +0200

    streamProcessor puts event data in S3

--- handler2.js
+++ handler2.js
@@ -10,8 +10,8 @@ module.exports.streamProcessor = (event, context, callback) => {
   const s3 = new AWS.S3();
   const params = {
       Bucket : 'music-service-dev-tempbucket-ll2ztsovqmpg',
-      Key : 'key',
-      Body : 'data'
+      Key : 'data.json',
+      Body : JSON.stringify(event)
   };
   s3.putObject(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
