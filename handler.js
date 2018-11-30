621aab0f407ea275b67cdd519591ea45560c94ee
Achilles Moraites <achimoraites@yahoo.gr>
2018-11-30 14:33:30 +0200

    refactored code to address bugs

--- handler.js
+++ handler.js
@@ -3,51 +3,56 @@
 const dynamoDb = require('./dynamodb');
 const uuid = require('uuid');
 
-module.exports.write = (event, context, callback) => {
-console.log('1:: Started write function ');
-// Customized params
-    const params = artist => {
-      return {
-        TableName: process.env.DYNAMODB_TABLE,
-        Item: {
-          id: uuid.v1(),
-          artist
-        }
-      };
-
+module.exports.write = async (event, context, callback) => {
+  console.log('1:: Started write function ');
+  // Customized params
+  const params = artist => {
+    return {
+      TableName: process.env.DYNAMODB_TABLE,
+      Item: {
+        id: uuid.v1(),
+        artist
+      }
     };
 
-// BUG: The for loop is syncronous but the db operation is not. Neither is setTimeout.
-// put 10 records
-    for(let i=0;i<10;i++) {
+  };
 
-      setTimeout(() => {
+/**
+ * Puts items in MUSIC table.
+ * @param {int} num1 The first number.
+ */
+async function put10Items() {
+  const results = [];
+  for (let i=0;i<10;i++) {
+    results.push(dynamoDb.put(params(`artist ${uuid.v4()}`)).promise());
+    console.log('Inserting element :', i);
+  }
+  return await Promise.all(results);
+}
 
-        dynamoDb.put(params(`artist ${uuid.v4()}`), (error) => {
-          if (error) {
-            console.error(error);
-            callback(null, {
-              statusCode: error.statusCode || 501,
-              headers: {
-                'Content-Type': 'text/plain'
-              },
-              body: 'Couldn\'t create the record item.'
-            });
-            return;
-          }
-      });
+  // BUG: The for loop is syncronous but the db operation is not. Neither is setTimeout.
+  // put 10 records
 
-         }, 10);
+  try {
+    put10Items();
+  } catch (error) {
+    console.error(error);
+    callback(null, {
+      statusCode: error.statusCode || 501,
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: 'Couldn\'t create the record item.'
+    });
+  }
 
-    }
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({message:'success'})
+  };
 
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(params.Item)
-    };
+  // BUG: This callback will be invoked before items are put and before any
+  // errors are returned from the db operations
+  callback(null, response);
 
-    // BUG: This callback will be invoked before items are put and before any
-    // errors are returned from the db operations
-    callback(null, response);
-
-    };
+};
